c53ebada7f75827b0132a7081045ea963119f004
tviivi <viivi.tiihonen@helsinki.fi>
2018-02-06 16:06:35 +0200

    tehtävä 3.7

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const express = require('express')
 const app = express()
 const bodyParser = require('body-parser')
+const morgan = require('morgan')
 
+app.use(morgan('tiny'))
 app.use(bodyParser.json())
 
 const aika = Date()
