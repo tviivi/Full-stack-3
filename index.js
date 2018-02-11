0e0c433954e18912613123bdfaf8ac9b313843f6
tviivi <viivi.tiihonen@helsinki.fi>
2018-02-11 19:00:15 +0200

    tehtävä 3.16

--- index.js
+++ index.js
@@ -82,10 +82,14 @@ let persons = [
   })
 
   app.delete('/api/persons/:id', (request, response) => {
-    const id = Number(request.params.id)
-    persons = persons.filter(person => person.id !== id)
-  
-    response.status(204).end()
+    Person
+    .findByIdAndRemove(request.params.id)
+    .then(result => {
+      response.status(204).end()
+    })
+    .catch(error => {
+      response.status(400).send({ error: 'malformatted id' })
+    })
   })
 
   app.post('/api/persons', (request, response) => {
