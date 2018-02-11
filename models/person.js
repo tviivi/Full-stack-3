70bf234bd603561af6b68bc575cb71d9b77ab70e
tviivi <viivi.tiihonen@helsinki.fi>
2018-02-11 19:15:20 +0200

    tehtävä 3.20

--- models/person.js
+++ models/person.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose')
 
-const url = 'mongodb://fullstack:sekred@ds229448.mlab.com:29448/puhelinluettelo'
+const url = 'mongodb://fullstack:*****@ds229448.mlab.com:29448/puhelinluettelo'
 
 mongoose.connect(url)
 
