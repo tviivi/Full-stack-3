1cda476e7f3e44c769fd72b7bb3fa4a6fdc6785e
tviivi <viivi.tiihonen@helsinki.fi>
2018-02-11 14:33:40 +0200

    väliaikapush

--- index.js
+++ index.js
@@ -44,9 +44,9 @@ let persons = [
     }
   ]
   
-  app.get('/', (req, res) => {
-    res.send('<h1>Hello World!</h1>')
-  })
+  //app.get('/', (req, res) => {
+  //  res.send('<h1>Hello World!</h1>')
+  //})
   
   app.get('/api/persons', (req, res) => {
     res.json(persons)
