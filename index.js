bb31f9ce33a1c765335c7c5e5e72ebe64fa6ff53
tviivi <viivi.tiihonen@helsinki.fi>
2018-02-11 11:49:54 +0200

    Initiate app.

--- index.js
+++ index.js
@@ -97,7 +97,7 @@ let persons = [
     response.json(person)
   })
 
-  const PORT = 3002
+  const PORT = process.env.PORT ||3002
   app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
   })
